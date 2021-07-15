7cfea471dd5d8f31c7f581e5a61f6f1458f59323
matzhananar <anr.matzhn@gmail.com>
2021-07-15 12:23:02 +0600

    15-07

---
+++ Махамбет/14-07t2.py
@@ -0,0 +1,13 @@
+a = input("")
+
+print(a+a)
+
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+print(a, "+", b, "=", a+b)
+
+pi = 3.14
+print("Hello world", 100, pi)
+print("hello world"+str(100+pi))
---
+++ Талгат/lesson1-15-07t1.py
@@ -0,0 +1,4 @@
+a = int(input())
+
+print("the next number for the number", a, "is", a+1)
+print("the previous number for the number", a, "is", a-1)
